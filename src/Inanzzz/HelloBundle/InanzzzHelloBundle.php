e87ebdaeae9ae8e1667ea755621091021ac770f9
Mehul Jain <somesh.jagarapu@gmail.com>
2018-09-01 15:57:09 +0530

    First commit

---
+++ src/Inanzzz/HelloBundle/InanzzzHelloBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Inanzzz\HelloBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class InanzzzHelloBundle extends Bundle
+{
+}
---
+++ src/Inanzzz/HelloBundle/Util/Hello.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Inanzzz\HelloBundle\Util;
+
+class Hello {
+    public function name($value){
+        return "hello".$value."!";
+    }
+}
